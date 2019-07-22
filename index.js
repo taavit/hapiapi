a75d3f582b1b9193dcc3979e873ab141e1d322f3
Dawid Królak <754920+taavit@users.noreply.github.com>
2019-07-22 11:47:32 +0200

    Updated dependencies

--- index.js
+++ index.js
@@ -1,49 +1,57 @@
-import {Server} from 'hapi';
-import {notFound} from 'boom';
+import Hapi from '@hapi/hapi';
+import { notFound } from 'boom';
 import Book from './src/model/book';
 
-const server = new Server(
-    {connections: {
-        routes: {
-            cors: true
-        }
-    }
-});
+const init = async () => {
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
 
-server.connection({
-    port: parseInt(process.env.PORT, 10) || 5000
-});
-
-server.route({
-    method: 'GET',
-    path: '/books',
-    handler: (request, reply) => Book.findAll().then((books) => reply(books))
-});
-
-server.route({
-    method: 'GET',
-    path: '/books/{id}',
-    handler: (request, reply) => Book.findById(request.params.id).then((book) => reply(book === null ? notFound('book not found') : book))
-});
-
-server.route({
-    method: 'PUT',
-    path: '/books/{id}',
-    handler: (request, reply) => Book.update(request.payload, {where: {id: request.params.id}}).then((book) => reply())
-});
-
-server.route({
-    method: 'POST',
-    path: '/books',
-    handler: (request, reply) => Book.create(request.payload).then((book) => reply(book))
-});
-
-server.route({
-    method: 'DELETE',
-    path: '/books/{id}',
-    handler: (request, reply) => Book.destroy({where: {id: request.params.id}}).then(() => reply())
-});
-
-server.start(() => {
+    server.route({
+        method: 'GET',
+        path: '/books',
+        handler: async () => await Book.findAll(),
+    });
+    
+    server.route({
+        method: 'GET',
+        path: '/books/{id}',
+        handler: async (request) => {
+            const book = await Book.findAll({ where: { id: request.params.id } });
+            if (book.length === 0) {
+                return notFound('Book not found');
+            }
+            return book;
+        },
+    });
+    
+    server.route({
+        method: 'PUT',
+        path: '/books/{id}',
+        handler: async (request, reply) => {
+            await Book.update(request.payload, {where: {id: request.params.id}});
+            return null;
+        },
+    });
+    
+    server.route({
+        method: 'POST',
+        path: '/books',
+        handler: async (request, reply) => await Book.create(request.payload),
+    });
+    
+    server.route({
+        method: 'DELETE',
+        path: '/books/{id}',
+        handler: async (request, reply) => {
+            await Book.destroy({where: {id: request.params.id}});
+            return null;
+        },
+    });
+    
+    await server.start();
     console.log('Server running at:', server.info.uri);
-});
+}
+
+ init();
--- src/sequelize.js
+++ src/sequelize.js
@@ -1,7 +1,7 @@
 import Sequelize from 'sequelize';
 
 const sequelize = new Sequelize(
-    process.env.DATABASE_URL || 'postgres://demo:demo@localhost:5432/demodb'
+    process.env.DATABASE_URL || 'postgres://demo:demo@localhost:5432/demo'
 );
 
 export default sequelize;
